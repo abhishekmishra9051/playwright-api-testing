c122623bd84c51bd87bc0c4f5450868b21034682
Abhishek Mishra <abhishekmishra9051@gmail.com>
2023-12-05 22:08:12 +0530

    reports-public-url

--- playwright.config.js
+++ playwright.config.js
@@ -2,13 +2,6 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'tests',
-
-  // Set test configuration options
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-
   // Reporter to use
   reporter: [
     ['html'],
